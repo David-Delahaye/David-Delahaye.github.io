7f3097b57440578e3897115f4e3921163d579f43
David Delahaye <delahaye3@hotmail.co.uk>
2020-08-21 22:01:20 +0100

    Updates

---
+++ precache-manifest.69991a15b61613a8378f67a33c90b768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da29dc77adbcc3c114e1f85249df951f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28bd8a9e93f3f8278ca1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28bd8a9e93f3f8278ca1",
+    "url": "/static/js/main.f15571f6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
