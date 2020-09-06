4d06bb22506e5f44b3ccbd32935a85981a2bf6c8
David Delahaye <delahaye3@hotmail.co.uk>
2020-09-06 21:46:11 +0100

    Updates

---
+++ precache-manifest.33989e4cc656f52318537a971540e896.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35638108c679ca82cf09478e2dbaeb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dbc3adac0215cd9ac65",
+    "url": "/static/css/main.1f6b5a37.chunk.css"
+  },
+  {
+    "revision": "2d267783029d76f026d0",
+    "url": "/static/js/2.81f6fd6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.81f6fd6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dbc3adac0215cd9ac65",
+    "url": "/static/js/main.04a7ba76.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "797960d3f1def3a80610a1e0eabdb0f1",
+    "url": "/static/media/MGResponsive.797960d3.png"
+  },
+  {
+    "revision": "f5dfa0223422ec8264c522bcf7a25998",
+    "url": "/static/media/UKHResponsive.f5dfa022.png"
+  }
+]);
