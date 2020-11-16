68c9b046d8a08c6fe52de64f68116fab7a5ea1b7
David Delahaye <delahaye3@hotmail.co.uk>
2020-11-16 22:52:39 +0000

    add google tag

--- pages/index.js
+++ pages/index.js
@@ -15,34 +15,39 @@ export default function Home() {
 
       <section>
         <div className="line">
-          <p>
-            <h3>Skills</h3>I Have been learning all I can about development
-            since Autumn 2019, Along the way I have gained knowledge of many{" "}
-            <strong>powerful technologies </strong>
-            and have built a vast array of projects by myself and with a Team, I
-            am looking forward to continue progressing and look forward to the
-            next steps of that journey. A list of technologies i am already
-            proficient with are as follows: -
-          </p>
+          <div>
+            <h3>Skills</h3>
+            <p>
+              I Have been learning all I can about development since Autumn
+              2019, Along the way I have gained knowledge of many{" "}
+              <strong>powerful technologies </strong>
+              and have built a vast array of projects by myself and with a Team,
+              I am looking forward to continue progressing and look forward to
+              the next steps of that journey. A list of technologies i am
+              already proficient with are as follows: -
+            </p>
+          </div>
 
           <div>
             <Image src="/laptop.png" width="1287" height="747" />
           </div>
         </div>
         <div className="line">
-          <p>
+          <div>
             <h3>Goals</h3>
-            My Goal for 2020/2021 is to begin working towards a full-time career
-            in <strong>web-development</strong> so i can spend more time towards
-            progressing myself and becoming a more competent developer. If you
-            have similar interests feel free to drop me a line on my contact
-            page, or feel free to take a look at some of my{" "}
-            <Link href="/work" scroll={false}>
-              <a>
-                <strong>past projects</strong>
-              </a>
-            </Link>
-          </p>
+            <p>
+              My Goal for 2020/2021 is to begin working towards a full-time
+              career in <strong>web-development</strong> so i can spend more
+              time towards progressing myself and becoming a more competent
+              developer. If you have similar interests feel free to drop me a
+              line on my contact page, or feel free to take a look at some of my{" "}
+              <Link href="/work" scroll={false}>
+                <a>
+                  <strong>past projects</strong>
+                </a>
+              </Link>
+            </p>
+          </div>
           <div>
             <Image src="/mobile.png" width="1287" height="747" />
           </div>
