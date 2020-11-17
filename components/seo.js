d06df87abc5ceee37cefcc4ec11a330e8aebf5da
David Delahaye <delahaye3@hotmail.co.uk>
2020-11-17 13:35:06 +0000

    add recaptcha

--- components/seo.js
+++ components/seo.js
@@ -5,6 +5,9 @@ export default function Seo({ title, description }) {
     <div>
       <Head>
         <title>{title}</title>
+        <script
+          src={`https://www.google.com/recaptcha/api.js?render=${process.env.reCAPTCHA_site_key}`}
+        ></script>
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.svg" />
--- pages/contact.js
+++ pages/contact.js
@@ -3,22 +3,34 @@ import Page from "../components/page";
 export default function Contact() {
   async function formSubmit(e) {
     e.preventDefault();
-    const body = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      message: e.target.message.value,
-    };
-    const response = await fetch("/api/mail", {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify(body),
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
   }
+
+  function formSubmit(e) {
+    e.preventDefault();
+    grecaptcha.ready(function () {
+      grecaptcha
+        .execute(process.env.reCAPTCHA_site_key, { action: "submit" })
+        .then(async function (token) {
+          console.log(token);
+          const body = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            message: e.target.message.value,
+          };
+          const response = await fetch("/api/mail", {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            headers: {
+              "Content-Type": "application/json",
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(body),
+          });
+          return response.json(); // parses JSON response into native JavaScript objects
+        });
+    });
+  }
+
   return (
     <main>
       <section>
